7099a6f4ac19730d88a49a71ab4980de6c359357
Ewerton dos anjos ribeiro <ewertondosanjosz@gmail.com>
2022-06-23 15:23:17 -0300

    Gerando numeros aleatorios com o crate rand

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rand::{self, Rng};
 use std::io::stdin;
 
 fn main() {
@@ -5,10 +6,13 @@ fn main() {
 
     println!("Qual seu chute?");
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
     //Cria a variavel que armazena o chute
     let mut chute = String::new();
 
     stdin().read_line(&mut chute).expect("Erro ao ler o input!");
 
+    println!("O Numero secreto era {}", secret_number);
     println!("Voce chutou {}", chute);
 }
