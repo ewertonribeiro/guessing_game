71eacc8bce30bbcc68fbd96356ce7a1d21ed32ae
Ewerton dos anjos ribeiro <ewertondosanjosz@gmail.com>
2022-06-24 13:11:25 -0300

    Comparando o chute o jogador com o numero secreto

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 use rand::{self, Rng};
-use std::io::stdin;
+use std::{cmp::Ordering, io::stdin};
 
 fn main() {
-    println!("Advinhe o numero Secreto");
-
-    println!("Qual seu chute?");
-
     let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Advinhe o numero Secreto");
+    println!("Qual seu chute?");
 
     //Cria a variavel que armazena o chute
     let mut chute = String::new();
-
     stdin().read_line(&mut chute).expect("Erro ao ler o input!");
+
+    //Usa a funcionalidade de Shadowing para converter o tipo de string para i:8
+    let chute: u8 = chute.trim().parse().expect("Por favor digite um numero!");
+
+    match chute.cmp(&secret_number) {
+        Ordering::Less => println!("Seu chute foi menor!"),
+        Ordering::Greater => println!("Seu chute foi maior!"),
+        Ordering::Equal => println!("Você acertou!!"),
+    }
 
     println!("O Numero secreto era {}", secret_number);
     println!("Voce chutou {}", chute);
